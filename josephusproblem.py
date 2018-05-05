c01999a9e75e6aa7884cff1c6c8a4dcbbcaeb382
Nicholas Wu <nickwu241@users.noreply.github.com>
2018-05-05 02:09:48 -0400

    Update josephusproblem.py

--- josephusproblem.py
+++ josephusproblem.py
@@ -1,12 +1,8 @@
 # https://en.wikipedia.org/wiki/Josephus_problem
 def josephus(n, k):
-    if n == 1:
-        return 1
-    else:
-        return (josephus(n-1, k) + k - 1) % n + 1
+    return 1 if n == 1 else (josephus(n-1, k) + k - 1) % n + 1
 
 n = 100
 k = 2
 print('Josephus Problem: n={:d}, k={:d}'.format(n, k))
 print('To be alive, start at position {:d}'.format(josephus(n, k)))
-
