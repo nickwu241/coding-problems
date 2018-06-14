f064a325311689a6d923bc1321ab28a6c2834c38
Nicholas Wu <nickwu241@users.noreply.github.com>
2018-06-14 01:43:16 -0400

    Update wordBreak.py

--- leetcode/wordBreak.py
+++ leetcode/wordBreak.py
@@ -6,17 +6,17 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
+        def setPossibleBreaks(start):
+            for w in wordDict:
+                end = start+len(w)
+                if end >= len(dp) or dp[end]:
+                    continue
+                if s[start:end] == w:
+                    dp[end] = True
+            
         dp = [False] * (len(s) + 1)
-        self.setPossibleBreaks(s, wordDict, 0, dp)
+        setPossibleBreaks(0)
         for i in xrange(1, len(dp)):
             if dp[i]:
-                self.setPossibleBreaks(s, wordDict, i, dp)
+                setPossibleBreaks(i)
         return dp[-1]
-    
-    def setPossibleBreaks(self, s, wordDict, start, dp):
-        for w in wordDict:
-            end = start+len(w)
-            if end >= len(dp) or dp[end]:
-                continue
-            if s[start:end] == w:
-                dp[end] = True
