5fcf00d8350b8808848b3ffd7085e16573d7a592
Nicholas Wu <nickwu241@users.noreply.github.com>
2018-06-21 22:30:35 -0400

    Create pushDominoes.py

---
+++ leetcode/pushDominoes.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/push-dominoes/description/
+class Solution(object):
+    def pushDominoes(self, dominoes):
+        """
+        :type dominoes: str
+        :rtype: str
+        """
+        last_letter = 'L'
+        last_letter_i = -1
+        list_dominoes = list(dominoes)
+        for i, dominoe in enumerate(list_dominoes + ['R']):
+            if dominoe == '.':
+                continue
+            
+            if dominoe == 'L' and last_letter == 'R': # R...L
+                lo = last_letter_i + 1
+                high = i -1
+                while lo < high:
+                    list_dominoes[lo] = 'R'
+                    list_dominoes[high] = 'L'
+                    lo += 1
+                    high -= 1
+            elif dominoe == 'L': # L...L
+                for j in range(last_letter_i + 1, i):
+                    list_dominoes[j] = 'L'
+            elif dominoe == 'R' and last_letter == 'R': # R...R
+                for j in range(last_letter_i + 1, i):
+                    list_dominoes[j] = 'R'
+            last_letter = dominoe
+            last_letter_i = i
+        return ''.join(list_dominoes)
