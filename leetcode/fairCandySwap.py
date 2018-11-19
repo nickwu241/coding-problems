142048db2a85573428e0871d1be239ce9b48021a
Nicholas Wu <nickwu241@gmail.com>
2018-11-19 00:37:12 -0800

    Create fairCandySwap.py

---
+++ leetcode/fairCandySwap.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/fair-candy-swap/description
+class Solution(object):
+    def fairCandySwap(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        sumA = 0
+        setA = set()
+        for candy_size in A:
+            sumA += candy_size
+            setA.add(candy_size)
+
+        diff = sum(B) - sumA
+        for candy_size in B:
+            if candy_size - (diff / 2) in setA:
+                return [candy_size - (diff / 2), candy_size]
