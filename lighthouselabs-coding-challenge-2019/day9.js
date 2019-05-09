2f956b5878d0bfde093fcd1bc426a9e05d3a36db
Nick Wu <nickwu241@gmail.com>
2019-05-09 08:29:19 -0700

    Finish day9

---
+++ lighthouselabs-coding-challenge-2019/day9.js
@@ -0,0 +1,38 @@
+function powerOn() {
+  ship.powerOn = true;
+}
+
+function countModules() {
+  return availableModules.length;
+}
+
+function countEssential() {
+  return availableModules.filter((module) => module.essential).length;
+}
+
+function loadModule(index) {
+  availableModules[index].enabled = true;
+  ship.modules.push(availableModules[index]);
+}
+
+function findModuleIndex(moduleName) {
+  return availableModules.findIndex(
+    (module) => module.essential && module.name === moduleName);
+}
+
+function resetLARRY() {
+  for (var i = 0; i < 10; i++) {
+    LARRY.quack();
+  }
+}
+
+function setMessage() {
+  radio.message = JSON.stringify(navigation);
+}
+
+loadModule(findModuleIndex('life-support'));
+loadModule(findModuleIndex('propulsion'));
+loadModule(findModuleIndex('navigation'));
+loadModule(findModuleIndex('communication'));
+resetLARRY();
+setMessage();
