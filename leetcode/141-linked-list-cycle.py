4653e3acf69c42447adb430d38008cbff8778aef
Nicholas Wu <nickwu241@gmail.com>
2019-06-09 19:06:20 -0700

    Create 141-linked-list-cycle.py

---
+++ leetcode/141-linked-list-cycle.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/linked-list-cycle/
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head:
+            return False
+        first = head
+        second = head.next
+        while second and second.next:
+            if first == second:
+                return True
+            first = first.next
+            second = second.next.next
+        return False
