721ab3e9d55fbdbdb74967d443378c057d16b054
Nick Wu <nickwu241@gmail.com>
2019-11-03 09:13:26 +0800

    Finish day1

---
+++ lighthouselabs-coding-challenge-2019-nov/day1.js
@@ -0,0 +1,9 @@
+const doorToDoor = (volunteers, neighbourhoods) => {
+    // Code here!
+
+    // Remember to return a value
+
+    // Want to see what's going on inside your function? You can use console.log() to debug and print variables to the console below.
+    return neighbourhoods.length / volunteers.length;
+}
+
