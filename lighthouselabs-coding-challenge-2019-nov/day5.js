bb4d918056c7155e59b4ecfdfc8cbfea1fb1c6a9
Nick Wu <nickwu241@gmail.com>
2019-11-07 12:42:44 +0800

    Finish day5
    Signed-off-by: Nick Wu <nickwu241@gmail.com>

---
+++ lighthouselabs-coding-challenge-2019-nov/day5.js
@@ -0,0 +1,10 @@
+const chooseStations = (stations) => {
+  // Code here!
+
+  // Remember to return a value!
+  return stations.filter(
+    station => station[1] >= 20 &&
+      (station[2] === 'school' || station[2] === 'community centre')
+  ).map(station => station[0]);
+}
+
